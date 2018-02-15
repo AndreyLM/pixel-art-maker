b4c0be7abe427b1de01b37dd550a83b01945ad09
andrew <kpi_lithium@ukr.net>
2018-02-15 11:41:59 +0200

    final js version

--- src/script.js
+++ src/script.js
@@ -1,84 +1,203 @@
 document.addEventListener("DOMContentLoaded", function(event) {
-    function init() {
-        var canvas = document.getElementsByClassName('canvas')[0];
-        var currentColor = document.getElementsByClassName('current-color-box')[0];
-        var colors = document.getElementsByClassName('colors')[0];
-        var canvasSize = document.getElementById('applyCanvasSize');
-        var colorPicker = document.getElementById('colorPicker');
-        var btnLoad = document.getElementById('btn-load');
-        var btnSave = document.getElementById('btn-save');
-        var brushColor = 'red';
-        var canvasHeight = 100;
-        var canvasWidth = 300;
+    var obj = {
 
-        currentColor.style.backgroundColor = brushColor;
+        brush: 'brush',
+        flood: 'flood',
+        matrix: [],
+        matrixCash: [],
+        pxls: document.getElementsByClassName('pxl'),
+        canvas : document.getElementsByClassName('canvas')[0],
+        currentColor : document.getElementsByClassName('current-color-box')[0],
+        colors : document.getElementsByClassName('colors')[0],
+        colorType: document.getElementById('color-type'),
+        canvasSize : document.getElementById('applyCanvasSize'),
+        colorPicker : document.getElementById('colorPicker'),
+        btnLoad : document.getElementById('btn-load'),
+        btnSave : document.getElementById('btn-save'),
+        btnClear : document.getElementById('btn-clear'),
+        paintColor : 'red',
+        fillingType : this.brush,
+        canvasHeight : document.getElementById('canvas-height'),
+        canvasWidth : document.getElementById('canvas-width'),
 
+        init: function() {
 
-        addEvents(canvas, canvasSize, colors, colorPicker, brushColor, currentColor, );
-
-        createCanvas(canvasWidth, canvasHeight, canvas);
-    }
-
-    function addEvents(canvas, canvasSize, colors, colorPicker, brushColor, currentColor) {
-        var mouseOverHandler = function (e) {
-            e.target.style.backgroundColor = brushColor;
-        };
-
-        colors.addEventListener('click', function (e) {
-            brushColor = window.getComputedStyle(e.target, null).getPropertyValue("background-color");
-            currentColor.style.backgroundColor = brushColor;
-
-        });
-
-        canvas.addEventListener('click', function (e) {
-            e.target.style.backgroundColor = brushColor;
-        });
-
-        canvas.addEventListener('mouseup', function (e) {
-            canvas.removeEventListener('mouseover', mouseOverHandler);
-        });
-
-        canvas.addEventListener('mousedown', function (e) {
-            canvas.addEventListener('mouseover', mouseOverHandler);
-        });
+            this.currentColor.style.backgroundColor = this.paintColor;
 
 
-        canvasSize.addEventListener('click', function (e) {
-            // alert('Click');
+            this.addEvents();
 
-            resizeCanvas(e.target, canvas);
-            e.preventDefault();
-        });
+            this.createCanvas(this.canvasWidth.value, this.canvasHeight.value);
+        },
 
-        colorPicker.addEventListener('change', function (e) {
-            brushColor = e.target.value;
-            currentColor.style.backgroundColor = e.target.value;
-        });
-    }
+        addEvents: function () {
+            var self = this;
+            var colorTypes = self.colorType.elements['color-fill'];
 
-    function createCanvas(width, height, parent) {
-        parent.innerHTML = '';
-        for(var i=1; i<=height/5; i++) {
-            var row = document.createElement('div');
-            row.className = 'row';
-
-            for (var j=1; j<=width/5; j++) {
-                var box = document.createElement('div');
-                box.className = 'pxl';
-                row.appendChild(box);
+            for(var i=0; i<colorTypes.length; i++) {
+                colorTypes[i].addEventListener('click', function (e) {
+                    self.fillingType = e.target.value;
+                })
             }
 
-            parent.appendChild(row);
+            var mouseOverHandler = function (e) {
+                e.target.style.backgroundColor = self.paintColor;
+            };
+
+            self.colors.addEventListener('click', function (e) {
+                self.paintColor = window.getComputedStyle(e.target, null).getPropertyValue("background-color");
+                self.currentColor.style.backgroundColor = self.paintColor;
+
+            });
+
+            self.canvas.addEventListener('click', function (e) {
+                if(self.fillingType === self.brush)
+                    e.target.style.backgroundColor = self.paintColor;
+                else {
+                    var exTime = Date.now();
+                    var color = window.getComputedStyle(e.target, null).getPropertyValue("background-color");
+                    self.makeMatrix();
+                    var node = self.getElement(parseInt(e.target.dataset.row), parseInt(e.target.dataset.col));
+                    self.floodFillCanvas.call(self, node, color, self.paintColor);
+                    console.log(Date.now() - exTime);
+                }
+            });
+
+            self.canvas.addEventListener('mouseup', function (e) {
+                self.canvas.removeEventListener('mouseover', mouseOverHandler);
+            });
+
+            self.canvas.addEventListener('mousedown', function (e) {
+                self.canvas.addEventListener('mouseover', mouseOverHandler);
+            });
+
+
+            self.canvasSize.addEventListener('click', function (e) {
+                self.createCanvas.call(self, self.canvasWidth.value, self.canvasHeight.value);
+                e.preventDefault();
+            });
+
+            self.colorPicker.addEventListener('change', function (e) {
+                self.paintColor = e.target.value;
+                self.currentColor.style.backgroundColor = e.target.value;
+            });
+
+            self.btnSave.addEventListener('click', function (e) {
+                self.saveCanvas.call(self, null);
+                e.preventDefault();
+            });
+
+            self.btnLoad.addEventListener('click', function (e) {
+                self.loadCanvas.call(self, null);
+                e.preventDefault();
+            });
+
+            self.btnClear.addEventListener('click', function (e) {
+                self.createCanvas.call(self, self.canvasWidth.value, self.canvasHeight.value);
+                e.preventDefault();
+            });
+        },
+
+        createCanvas: function(width, height) {
+            this.canvas.innerHTML = '';
+            for(var i=1; i<=height/5; i++) {
+                var row = document.createElement('div');
+                row.className = 'row';
+
+                for (var j=1; j<=width/5; j++) {
+                    var box = document.createElement('div');
+                    box.className = 'pxl color3';
+                    box.dataset.row = i;
+                    box.dataset.col = j;
+                    row.appendChild(box);
+                }
+
+                this.canvas.appendChild(row);
+            }
+        },
+
+        saveCanvas: function () {
+              localStorage.setItem("canvas", this.canvas.innerHTML);
+        },
+
+        loadCanvas: function () {
+            this.canvas.innerHTML = localStorage.getItem("canvas");
+        },
+
+        floodFillCanvas: function (node, targetColor, replacementColor) {
+            console.log('0');
+
+            if(!node) {
+                return false;
+            }
+
+
+            if(targetColor === replacementColor) {
+                return false;
+            }
+
+            if(window.getComputedStyle(node.el, null).getPropertyValue("background-color") !== targetColor) {
+                return false;
+            }
+
+            node.el.style.backgroundColor = replacementColor;
+
+            this.floodFillCanvas(
+                this.getElement(parseInt(node.row)+1, parseInt(node.col)),
+                targetColor, replacementColor
+            );
+
+            this.floodFillCanvas(
+                this.getElement(parseInt(node.row)-1, parseInt(node.col)),
+                targetColor, replacementColor
+            );
+
+
+            this.floodFillCanvas(
+                this.getElement(parseInt(node.row), parseInt(node.col) - 1),
+                targetColor, replacementColor
+            );
+
+
+            this.floodFillCanvas(
+                this.getElement(parseInt(node.row), parseInt(node.col) + 1),
+                targetColor, replacementColor
+            );
+
+        },
+
+        makeMatrix: function() {
+            this.matrix = [];
+            this.matrixCash = [];
+
+            for(var i=0; i<this.pxls.length; i++) {
+                var obj = {
+                        row : parseInt(this.pxls[i].dataset.row),
+                        col : parseInt(this.pxls[i].dataset.col),
+                        color : window.getComputedStyle(this.pxls[i], null).getPropertyValue("background-color"),
+                        el : this.pxls[i]
+                };
+
+                this.matrix.push(obj);
+            }
+        },
+
+        getElement: function (row, col) {
+
+              for (var i=0; i<this.matrix.length; i++) {
+                  if(parseInt(this.matrix[i].row) === row && parseInt(this.matrix[i].col) === col) {
+                      return this.matrix[i];
+                  }
+              }
+
+              return null;
         }
-    }
+    };
 
-    function resizeCanvas(obj, parent) {
-        var width = document.getElementById('canvas-width').value;
-        var height = document.getElementById('canvas-height').value;
-        createCanvas(width, height, parent);
-    }
 
-init();
+
+
+    obj.init();
 });
 
 
